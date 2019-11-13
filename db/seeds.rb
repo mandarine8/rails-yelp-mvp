2a204f74314b7caa072f9d487015226e1eb78f3a
Amandine Car <caramandine@gmail.com>
2019-11-13 16:54:37 -0500

    restaurant and reviews controllers added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create([{ name: 'Tom Yum', address: 'China', phone_number: '5145034455', category: 'chinese' },
+{ name: 'Mamma Mia', address: 'Roma', phone_number: '5145555511', category: 'italian' },
+{ name: 'Kinoya Izakaya', address: 'Tokyo', phone_number: '5145772030', category: 'japanese' },
+{ name: 'Le bistrot', address: 'Paris', phone_number: '5145556688', category: 'french' },
+{ name: 'Ptite frite', address: 'Bruxelles', phone_number: '5145039988', category: 'belgian' }])
